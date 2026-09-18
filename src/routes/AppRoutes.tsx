dc8e70f1321247e5afb0b9fc26220fa07b2b1018
oscarsebastianbarriosfernandez-hue <oscarsebastianbarriosfernandez@gmail.com>
2026-09-17 22:24:43 -0400

    Proteger ruta principal

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,27 @@
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
-
-
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import { authRepository } from "../repositories/authRepository";
 
+function ProtectedHome() {
+  return authRepository.isAuthenticated() ? <HomePage /> : <Navigate to="/login" replace />;
+}
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<ProtectedHome />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default AppRoutes;
